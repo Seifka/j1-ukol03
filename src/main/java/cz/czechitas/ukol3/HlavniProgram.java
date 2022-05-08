58d6e8ba0ad2af5bc79471a6159470841bcaeaaa
seifka <seifertovak@centrum.cz>
2022-05-08 13:54:22 +0200

    set,get, to String

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -1,5 +1,10 @@
 package cz.czechitas.ukol3;
 
+import cz.czechitas.ukol3.model.Disk;
+import cz.czechitas.ukol3.model.Pamet;
+import cz.czechitas.ukol3.model.Pocitac;
+import cz.czechitas.ukol3.model.Procesor;
+
 /**
  * Spouštěcí třída celého programu
  */
@@ -7,6 +12,13 @@ public class HlavniProgram {
 
     public static void main(String... args) {
         //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
+
+        Disk disk = new Disk();
+        Pamet pamet = new Pamet();
+        Procesor procesor = new Procesor();
+        Pocitac pocitac = new Pocitac();
+
+
         System.out.println("Program spuštěn.");
     }
 
---
+++ src/main/java/cz/czechitas/ukol3/model/Pocitac.java
@@ -0,0 +1,50 @@
+package cz.czechitas.ukol3.model;
+
+public class Pocitac {
+    private boolean jeZapnuty;
+    private Procesor cpu;
+    private Pamet ram;
+    private Disk pevnyDisk;
+
+    public boolean isJeZapnuty() {
+        return jeZapnuty;
+    }
+
+    public void setJeZapnuty(boolean jeZapnuty) {
+        this.jeZapnuty = jeZapnuty;
+    }
+
+    public Procesor getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(Procesor cpu) {
+        this.cpu = cpu;
+    }
+
+    public Pamet getRam() {
+        return ram;
+    }
+
+    public void setRam(Pamet ram) {
+        this.ram = ram;
+    }
+
+    public Disk getPevnyDisk() {
+        return pevnyDisk;
+    }
+
+    public void setPevnyDisk(Disk pevnyDisk) {
+        this.pevnyDisk = pevnyDisk;
+    }
+
+    @Override
+    public String toString() {
+        return "Pocitac{" +
+                "jeZapnuty=" + jeZapnuty +
+                ", cpu=" + cpu +
+                ", ram=" + ram +
+                ", pevnyDisk=" + pevnyDisk +
+                '}';
+    }
+}
